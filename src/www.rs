b9e185f0ba393f5542e3b6d1823266ece2516826
Truman Kilen <t@kilen.me>
2022-03-23 17:57:56 -0500

    Simplify SQL state

--- src/www.rs
+++ src/www.rs
@@ -1,8 +1,6 @@
 use sqlx::sqlite::SqlitePool;
 use serde::{Deserialize, Serialize};
 
-use std::env;
-
 use anyhow::Result;
 
 use trillium::{conn_unwrap, Conn, State, Handler};
@@ -12,34 +10,29 @@ use trillium_static_compiled::static_compiled;
 use maud::{DOCTYPE, html, PreEscaped};
 
 pub async fn run_web_server() -> Result<()>{
-    //trillium_tokio::run_async(|conn: trillium::Conn| async move {
-        //conn.ok("hello from trillium!")
-    //}).await;
-
-    let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
-    let db = DB::new(pool);
-
     trillium_tokio::config()
-        .run_async(app(db)).await;
-
+        .run_async(app()).await;
     Ok(())
 }
 
-#[derive(Debug, Clone)]
-struct DB(std::sync::Arc<tokio::sync::Mutex<SqlitePool>>);
-impl DB {
-    fn new(pool: SqlitePool) -> DB {
-        DB(std::sync::Arc::new(tokio::sync::Mutex::new(pool)))
-    }
-}
-
-fn app<'a>(db: DB) -> impl Handler {
+fn app<'a>() -> impl Handler {
     (
         Logger::new(),
-        State::new(db),
+        trillium::Init::new(|_| async move {
+            let db = SqlitePool::connect(&std::env::var("DATABASE_URL").unwrap())
+                .await
+                .unwrap();
+            State::new(db)
+        }),
         router(),
         static_compiled!("./public"),
     )
+}
+
+fn router() -> impl Handler {
+    Router::new()
+        .get("/", get_servers)
+        .get("/server/:time/:lobby_id", get_server)
 }
 
 trait MaudConnExt {
@@ -71,128 +64,97 @@ struct Mod {
     url: Option<String>,
 }
 
-fn router() -> impl Handler {
-    Router::new()
-        .get("/", |mut conn: Conn| async move {
-            let mut db = conn.state_mut::<DB>().unwrap().0.lock().await.acquire().await.unwrap();
-            let res = sqlx::query!(
-                r#"SELECT time,
-                    datetime(time, 'unixepoch', 'localtime') AS "time_formatted!: String",
-                    lobby_id,
-                    diff,
-                    region,
-                    host_user_id,
-                    server_name,
-                    (SELECT json_group_array(json_object('id', mod_id, 'category', category, 'name', name, 'url', url)) FROM
-                        (SELECT mod_id, category, name, url
-                        FROM server_mod
-                        JOIN mod USING(mod_id)
-                        WHERE
-                            server_mod.time = server.time
-                            AND server_mod.lobby_id = server.lobby_id
-                            AND category != 0
-                        ORDER BY category)
-                    ) AS mods
-                    FROM server
-                    WHERE diff = 4
-                    ORDER BY time
-                    DESC LIMIT 1000;
-                "#
-            )
-            .fetch_all(&mut db)
-            .await.unwrap();
+async fn get_servers(conn: Conn) -> Conn {
+    let pool = conn.state::<SqlitePool>().unwrap();
+    let res = sqlx::query!(
+        r#"SELECT time,
+            datetime(time, 'unixepoch', 'localtime') AS "time_formatted!: String",
+            lobby_id,
+            diff,
+            region,
+            host_user_id,
+            server_name,
+            (SELECT json_group_array(json_object('id', mod_id, 'category', category, 'name', name, 'url', url)) FROM
+                (SELECT mod_id, category, name, url
+                FROM server_mod
+                JOIN mod USING(mod_id)
+                WHERE
+                    server_mod.time = server.time
+                    AND server_mod.lobby_id = server.lobby_id
+                    AND category != 0
+                ORDER BY category)
+            ) AS mods
+            FROM server
+            WHERE diff = 4
+            ORDER BY time
+            DESC LIMIT 1000;
+        "#
+    )
+    .fetch_all(pool)
+    .await.unwrap();
 
-            /*
-            let servers: Vec<Server> = res.into_iter().map(|r| Ok(Server {
-                time: r.time.ok_or(Err("Malformed time"))?,
-                lobby_id: r.lobby_id,
-                difficulty: r.diff,
-                region: r.region,
-                host_user_id: r.host_user_id,
-                server_name: r.server_name,
-                mods: r.mods.map_or_else(|| Ok(vec![]), |m| {
-                    serde_json::from_str::<Vec<Mod>>(&m)
-                })?
-            })).collect()?;
-            */
+    let servers: Vec<Server> = res.into_iter().map(|r| Server {
+        time: r.time,
+        time_formatted: r.time_formatted,
+        lobby_id: r.lobby_id,
+        difficulty: r.diff,
+        region: r.region,
+        host_user_id: r.host_user_id,
+        server_name: r.server_name,
+        mods: r.mods.map_or_else(|| Ok(vec![]), |m| {
+            serde_json::from_str::<Vec<Mod>>(&m)
+        }).unwrap()
+    }).collect();
 
-            let servers: Vec<Server> = res.into_iter().map(|r| Server {
-                time: r.time,
-                time_formatted: r.time_formatted,
-                lobby_id: r.lobby_id,
-                difficulty: r.diff,
-                region: r.region,
-                host_user_id: r.host_user_id,
-                server_name: r.server_name,
-                mods: r.mods.map_or_else(|| Ok(vec![]), |m| {
-                    serde_json::from_str::<Vec<Mod>>(&m)
-                }).unwrap()
-            }).collect();
+    conn.render(render_servers(servers))
+}
+async fn get_server(conn: Conn) -> Conn {
+    let time = conn_unwrap!(conn.param("time"), conn).to_owned();
+    let lobby_id = conn_unwrap!(conn.param("lobby_id"), conn).to_owned();
 
-            conn.render(render_servers(servers))
-        })
-        .get("/server/:time/:lobby_id", |mut conn: Conn| async move {
-            let time = conn_unwrap!(conn.param("time"), conn).to_owned();
-            let lobby_id = conn_unwrap!(conn.param("lobby_id"), conn).to_owned();
+    let pool = conn.state::<SqlitePool>().unwrap();
+    let res = sqlx::query!(
+        r#"SELECT time,
+            datetime(time, 'unixepoch', 'localtime') AS "time_formatted!: String",
+            lobby_id,
+            diff,
+            region,
+            host_user_id,
+            server_name,
+            (SELECT json_group_array(json_object('id', mod_id, 'category', category, 'name', name, 'url', url)) FROM
+                (SELECT mod_id, category, name, url
+                FROM server_mod
+                JOIN mod USING(mod_id)
+                WHERE
+                    server_mod.time = server.time
+                    AND server_mod.lobby_id = server.lobby_id
+                    AND category != 0
+                ORDER BY category)
+            ) AS "mods?: String"
+            FROM server
+            WHERE server.time = ? AND server.lobby_id = ?
+            ORDER BY time
+        "#,
+        time,
+        lobby_id,
+    )
+    .fetch_all(pool)
+    .await.unwrap();
 
-            let mut db = conn.state_mut::<DB>().unwrap().0.lock().await.acquire().await.unwrap();
-            let res = sqlx::query!(
-                r#"SELECT time,
-                    datetime(time, 'unixepoch', 'localtime') AS "time_formatted!: String",
-                    lobby_id,
-                    diff,
-                    region,
-                    host_user_id,
-                    server_name,
-                    (SELECT json_group_array(json_object('id', mod_id, 'category', category, 'name', name, 'url', url)) FROM
-                        (SELECT mod_id, category, name, url
-                        FROM server_mod
-                        JOIN mod USING(mod_id)
-                        WHERE
-                            server_mod.time = server.time
-                            AND server_mod.lobby_id = server.lobby_id
-                            AND category != 0
-                        ORDER BY category)
-                    ) AS "mods?: String"
-                    FROM server
-                    WHERE server.time = ? AND server.lobby_id = ?
-                    ORDER BY time
-                "#,
-                time,
-                lobby_id,
-            )
-            .fetch_all(&mut db)
-            .await.unwrap();
+    let servers: Vec<Server> = res.into_iter().map(|r| Server {
+        time: r.time,
+        time_formatted: r.time_formatted,
+        lobby_id: r.lobby_id,
+        difficulty: r.diff,
+        region: r.region,
+        host_user_id: r.host_user_id,
+        server_name: r.server_name,
+        mods: r.mods.map_or_else(|| Ok(vec![]), |m| {
+            serde_json::from_str::<Vec<Mod>>(&m)
+        }).unwrap()
+    }).collect();
 
-            /*
-            let servers: Vec<Server> = res.into_iter().map(|r| Ok(Server {
-                time: r.time.ok_or(Err("Malformed time"))?,
-                lobby_id: r.lobby_id,
-                difficulty: r.diff,
-                region: r.region,
-                host_user_id: r.host_user_id,
-                server_name: r.server_name,
-                mods: r.mods.map_or_else(|| Ok(vec![]), |m| {
-                    serde_json::from_str::<Vec<Mod>>(&m)
-                })?
-            })).collect()?;
-            */
-
-            let servers: Vec<Server> = res.into_iter().map(|r| Server {
-                time: r.time,
-                time_formatted: r.time_formatted,
-                lobby_id: r.lobby_id,
-                difficulty: r.diff,
-                region: r.region,
-                host_user_id: r.host_user_id,
-                server_name: r.server_name,
-                mods: r.mods.map_or_else(|| Ok(vec![]), |m| {
-                    serde_json::from_str::<Vec<Mod>>(&m)
-                }).unwrap()
-            }).collect();
-
-            conn.render(render_servers(servers))
-        })
+    conn.render(render_servers(servers))
 }
 
 fn render_servers(servers: Vec<Server>) -> PreEscaped<String> {
