4ad1bcf591bdc9948231fd04301ae3a4ba3df650
Truman Kilen <t@kilen.me>
2022-03-23 17:42:53 -0500

    Add direct link to individual servers

--- src/www.rs
+++ src/www.rs
@@ -5,9 +5,9 @@ use std::env;
 
 use anyhow::Result;
 
-use trillium::{Conn, State, Handler};
+use trillium::{conn_unwrap, Conn, State, Handler};
 use trillium_logger::Logger;
-use trillium_router::Router;
+use trillium_router::{Router, RouterConnExt};
 use trillium_static_compiled::static_compiled;
 use maud::{DOCTYPE, html, PreEscaped};
 
@@ -52,117 +52,226 @@ impl MaudConnExt for Conn {
     }
 }
 
+struct Server {
+    time: i64,
+    time_formatted: String,
+    lobby_id: String,
+    difficulty: i64,
+    region: String,
+    host_user_id: String,
+    server_name: String,
+    mods: Vec<Mod>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Mod {
+    id: i64,
+    category: Option<i32>,
+    name: Option<String>,
+    url: Option<String>,
+}
+
 fn router() -> impl Handler {
     Router::new()
         .get("/", |mut conn: Conn| async move {
-            let html = {
-                let mut db = conn.state_mut::<DB>().unwrap().0.lock().await.acquire().await.unwrap();
-                let res = sqlx::query!(
-                    r#"SELECT datetime(time, 'unixepoch', 'localtime') AS time,
-                        lobby_id,
-                        diff,
-                        region,
-                        host_user_id,
-                        server_name,
-                        (SELECT json_group_array(json_object('id', mod_id, 'category', category, 'name', name, 'url', url)) FROM
-                            (SELECT mod_id, category, name, url
-                            FROM server_mod
-                            JOIN mod USING(mod_id)
-                            WHERE
-                                server_mod.time = server.time
-                                AND server_mod.lobby_id = server.lobby_id
-                                AND category != 0
-                            ORDER BY category)
-                        ) AS mods
-                        FROM server
-                        WHERE diff = 4
-                        ORDER BY time
-                        DESC LIMIT 1000;
-                    "#
-                )
-                .fetch_all(&mut db)
-                .await.unwrap();
+            let mut db = conn.state_mut::<DB>().unwrap().0.lock().await.acquire().await.unwrap();
+            let res = sqlx::query!(
+                r#"SELECT time,
+                    datetime(time, 'unixepoch', 'localtime') AS "time_formatted!: String",
+                    lobby_id,
+                    diff,
+                    region,
+                    host_user_id,
+                    server_name,
+                    (SELECT json_group_array(json_object('id', mod_id, 'category', category, 'name', name, 'url', url)) FROM
+                        (SELECT mod_id, category, name, url
+                        FROM server_mod
+                        JOIN mod USING(mod_id)
+                        WHERE
+                            server_mod.time = server.time
+                            AND server_mod.lobby_id = server.lobby_id
+                            AND category != 0
+                        ORDER BY category)
+                    ) AS mods
+                    FROM server
+                    WHERE diff = 4
+                    ORDER BY time
+                    DESC LIMIT 1000;
+                "#
+            )
+            .fetch_all(&mut db)
+            .await.unwrap();
 
-                #[derive(Serialize, Deserialize)]
-                struct Mod {
-                    id: i64,
-                    category: Option<i32>,
-                    name: Option<String>,
-                    url: Option<String>,
-                }
+            /*
+            let servers: Vec<Server> = res.into_iter().map(|r| Ok(Server {
+                time: r.time.ok_or(Err("Malformed time"))?,
+                lobby_id: r.lobby_id,
+                difficulty: r.diff,
+                region: r.region,
+                host_user_id: r.host_user_id,
+                server_name: r.server_name,
+                mods: r.mods.map_or_else(|| Ok(vec![]), |m| {
+                    serde_json::from_str::<Vec<Mod>>(&m)
+                })?
+            })).collect()?;
+            */
 
-                html! {
-                    html lang="en" {
-                        (DOCTYPE)
-                        head {
-                            meta charset="utf-8";
-                            meta name="viewport" content="width=device-width, initial-scale=1";
-                            link href="/static/css/bootstrap.min.css" rel="stylesheet";
-                            style {
-                                (PreEscaped(r#"
-                                    body > ul {
-                                        max-width: 700px;
-                                        width: auto;
-                                        margin: 0 auto;
-                                    }
-                                "#))
-                            }
+            let servers: Vec<Server> = res.into_iter().map(|r| Server {
+                time: r.time,
+                time_formatted: r.time_formatted,
+                lobby_id: r.lobby_id,
+                difficulty: r.diff,
+                region: r.region,
+                host_user_id: r.host_user_id,
+                server_name: r.server_name,
+                mods: r.mods.map_or_else(|| Ok(vec![]), |m| {
+                    serde_json::from_str::<Vec<Mod>>(&m)
+                }).unwrap()
+            }).collect();
+
+            conn.render(render_servers(servers))
+        })
+        .get("/server/:time/:lobby_id", |mut conn: Conn| async move {
+            let time = conn_unwrap!(conn.param("time"), conn).to_owned();
+            let lobby_id = conn_unwrap!(conn.param("lobby_id"), conn).to_owned();
+
+            let mut db = conn.state_mut::<DB>().unwrap().0.lock().await.acquire().await.unwrap();
+            let res = sqlx::query!(
+                r#"SELECT time,
+                    datetime(time, 'unixepoch', 'localtime') AS "time_formatted!: String",
+                    lobby_id,
+                    diff,
+                    region,
+                    host_user_id,
+                    server_name,
+                    (SELECT json_group_array(json_object('id', mod_id, 'category', category, 'name', name, 'url', url)) FROM
+                        (SELECT mod_id, category, name, url
+                        FROM server_mod
+                        JOIN mod USING(mod_id)
+                        WHERE
+                            server_mod.time = server.time
+                            AND server_mod.lobby_id = server.lobby_id
+                            AND category != 0
+                        ORDER BY category)
+                    ) AS "mods?: String"
+                    FROM server
+                    WHERE server.time = ? AND server.lobby_id = ?
+                    ORDER BY time
+                "#,
+                time,
+                lobby_id,
+            )
+            .fetch_all(&mut db)
+            .await.unwrap();
+
+            /*
+            let servers: Vec<Server> = res.into_iter().map(|r| Ok(Server {
+                time: r.time.ok_or(Err("Malformed time"))?,
+                lobby_id: r.lobby_id,
+                difficulty: r.diff,
+                region: r.region,
+                host_user_id: r.host_user_id,
+                server_name: r.server_name,
+                mods: r.mods.map_or_else(|| Ok(vec![]), |m| {
+                    serde_json::from_str::<Vec<Mod>>(&m)
+                })?
+            })).collect()?;
+            */
+
+            let servers: Vec<Server> = res.into_iter().map(|r| Server {
+                time: r.time,
+                time_formatted: r.time_formatted,
+                lobby_id: r.lobby_id,
+                difficulty: r.diff,
+                region: r.region,
+                host_user_id: r.host_user_id,
+                server_name: r.server_name,
+                mods: r.mods.map_or_else(|| Ok(vec![]), |m| {
+                    serde_json::from_str::<Vec<Mod>>(&m)
+                }).unwrap()
+            }).collect();
+
+            conn.render(render_servers(servers))
+        })
+}
+
+fn render_servers(servers: Vec<Server>) -> PreEscaped<String> {
+    html! {
+        html lang="en" {
+            (DOCTYPE)
+            head {
+                meta charset="utf-8";
+                meta name="viewport" content="width=device-width, initial-scale=1";
+                link href="/static/css/bootstrap.min.css" rel="stylesheet";
+                style {
+                    (PreEscaped(r#"
+                        body > ul {
+                            max-width: 700px;
+                            width: auto;
+                            margin: 0 auto;
                         }
-                        body {
-                            ul.list-group {
-                                @for l in res {
-                                    li.list-group-item {
-                                        div.d-flex."gap-2"."w-100".justify-content-between {
-                                            div {
-                                                h6 {
-                                                    (format!("Hazard {}", l.diff + 1))
-                                                    " - "
-                                                    a href=(format!("steam://joinlobby/548430/{}/{}", l.lobby_id, l.host_user_id)) {
-                                                        (l.server_name)
-                                                    }
-                                                }
-                                                p."mb-0"."opacity-75" {
-                                                    a href=(format!("https://steamcommunity.com/profiles/{}", l.host_user_id)) {
-                                                        "Steam profile"
-                                                    }
-                                                    @if let Some(json) = l.mods {
-                                                        @if let Ok(mods) = serde_json::from_str::<Vec<Mod>>(&json) {
-                                                            ul {
-                                                                @for m in mods {
-                                                                    li {
-                                                                        @if let (Some(category), Some(url), Some(name)) = (m.category, m.url, m.name) {
-                                                                            (match category {
-                                                                                0 => "Verified",
-                                                                                1 => "Approved",
-                                                                                2 => "Sandbox",
-                                                                                _ => "Unknown"
-                                                                            })
-                                                                            " - "
-                                                                            a href=(url) {
-                                                                                (name)
-                                                                            }
-                                                                        } @else {
-                                                                            "Hidden mod ("(m.id)")"
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
+                    "#))
+                }
+            }
+            body {
+                ul.list-group {
+                    @for server in servers {
+                        (render_server(server))
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn render_server(server: Server) -> PreEscaped<String> {
+    html! {
+        li.list-group-item {
+            div.d-flex."gap-2"."w-100".justify-content-between {
+                div {
+                    h6 {
+                        (format!("Hazard {}", server.difficulty + 1))
+                        " - "
+                        a href=(format!("steam://joinlobby/548430/{}/{}", server.lobby_id, server.host_user_id)) {
+                            (server.server_name)
+                        }
+                    }
+                    p."mb-0"."opacity-75" {
+                        a href=(format!("https://steamcommunity.com/profiles/{}", server.host_user_id)) {
+                            "Steam profile"
+                        }
+                        " - "
+                        a href=(format!("/server/{}/{}", server.time, server.lobby_id)) {
+                            "Link"
+                        }
+                        ul {
+                            @for m in server.mods {
+                                @if let Some(category) = m.category {
+                                    li {
+                                        (match category {
+                                            0 => "Verified",
+                                            1 => "Approved",
+                                            2 => "Sandbox",
+                                            _ => "Unknown"
+                                        })
+                                        " - "
+                                        @if let (Some(url), Some(name)) = (m.url, m.name) {
+                                            a href=(url) {
+                                                (name)
                                             }
-                                            small."opacity-50".text-nowrap {
-                                                (l.time.unwrap())
-                                            }
+                                        } @else {
+                                            "Hidden mod ("(m.id)")"
                                         }
                                     }
                                 }
                             }
                         }
-                        //(res.iter().map(|r| format!("{:?}", r)).join("\n"))
                     }
                 }
-            };
-            conn.render(html)
-        })
+                small."opacity-50".text-nowrap {
+                    (server.time_formatted)
+                }
+            }
+        }
+    }
 }
