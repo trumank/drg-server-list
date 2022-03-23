2d0880012f0c51527dc7a0a50040b050a70946a2
Truman Kilen <t@kilen.me>
2022-03-23 14:41:38 -0500

    Add CLI arguments to run specific actions

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,44 @@ use trillium_router::Router;
 use trillium_static_compiled::static_compiled;
 use maud::{DOCTYPE, html, PreEscaped};
 
+use clap::arg;
+
 use anyhow::Result;
 
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::env;
 
+#[derive(Clone, Copy)]
+struct Config {
+    poll_servers: bool,
+    poll_mods: bool,
+    www: bool,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
+
+    let matches = clap::Command::new("DRGServerList")
+        .about("Standalone DRG server list")
+        .arg(arg!(-m --"poll-mods"    "Poll and updated cached mod information"))
+        .arg(arg!(-s --"poll-servers" "Poll current server information"))
+        .arg(arg!(-w --"www"          "Run web server"))
+        .group(clap::ArgGroup::new("polling")
+               .args(&["poll-mods", "poll-servers"])
+               .conflicts_with("www")
+               .multiple(true))
+        .group(clap::ArgGroup::new("action")
+               .args(&["poll-mods", "poll-servers", "www"])
+               .multiple(true)
+               .required(true))
+        .get_matches();
+
+    let config = Config {
+        poll_mods: matches.is_present("poll-mods"),
+        poll_servers: matches.is_present("poll-servers"),
+        www: matches.is_present("www"),
+    };
 
     let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
 
@@ -26,10 +56,16 @@ async fn main() -> Result<()> {
 
     println!("{}", time);
 
-    //update_server_list(&pool, time).await?;
-    //update_mods(&pool).await?;
+    if config.poll_servers {
+        update_server_list(&pool, time).await?;
+    }
+    if config.poll_mods {
+        update_mods(&pool).await?;
+    }
 
-    run_web_server().await?;
+    if config.www {
+        run_web_server().await?;
+    }
 
     Ok(())
 }
